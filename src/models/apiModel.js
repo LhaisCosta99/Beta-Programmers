05331bf81af994332a1effb8244d5d337b959fea
RESN036 <lhaisferreirada@gmail.com>
2022-06-02 18:49:14 -0300

    criacao do  formulario inscriçao

---
+++ src/models/apiModel.js
@@ -0,0 +1,9 @@
+class Endereco{
+    constructor(rua,bairro,cidade,estado){
+        this.rua = rua
+        this.bairro = bairro
+        this.cidade = cidade
+        this.estado = estado
+        this.pais = 'Brasil'
+    }
+}
