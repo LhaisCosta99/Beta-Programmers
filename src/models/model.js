1feb0ecc92e4726a321fe16033406c662f0b57f5
RESN036 <lhaisferreirada@gmail.com>
2022-06-04 15:41:11 -0300

    associados eventos na pagina cadastro

--- src/models/model.js
+++ src/models/model.js
@@ -1,7 +1,8 @@
 class Cadastro {
-    constructor(nome,sobrenome,nascimento,cep,rua,bairro,cidade,estado,pais){
+    constructor(nome,sobrenome,cpf,nascimento,cep,rua,bairro,cidade,estado,pais,botao){
         this.nome = nome
         this.sobrenome = sobrenome
+        this.cpf = cpf
         this.nascimento = nascimento
         this.cep = cep
         this.rua = rua
@@ -9,5 +10,6 @@ class Cadastro {
         this.cidade = cidade
         this.estado = estado
         this.pais = pais
+        this.botao = botao
     }
 }
